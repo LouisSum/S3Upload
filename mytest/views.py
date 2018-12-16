8a574cffae9be06d2dbdc621a2d9f4ea905ea21d
LouisSum <zhangzs88@163.com>
2018-12-16 12:19:51 +0800

    upload files from anywhere

--- mytest/views.py
+++ mytest/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
 from boto3.session import Session
 import json
 
@@ -13,18 +14,21 @@ def index(request):
                       aws_secret_access_key=aws_secret, region_name='xxx')
     s3 = session.resource('s3')
     client = session.client('s3')
-    if request.method == "POST":
-        bucket = request.POST.get("bucket", None)
-        objFile = str(request.FILES["btn_file"])
-        data = open(objFile, 'rb')
-        cre_mul_upload = client.create_multipart_upload(Bucket=bucket, Key=objFile)
-        upload = client.upload_part(Bucket=bucket, Key=objFile, UploadId=cre_mul_upload["UploadId"], PartNumber=1, Body=data)
-        com_mul_upload = client.complete_multipart_upload(Bucket=bucket, Key=objFile, UploadId=cre_mul_upload["UploadId"],
-                                                          MultipartUpload={ 'Parts': [{'ETag': upload['ETag'], 'PartNumber': 1},]})
+    if request.method == 'POST':
+        bucket = request.POST.get('bucket', None)
+#        objFile = request.FILES["btn_file"]
+        objFile = request.FILES.get('btn_file', None)
+        if objFile is None:
+            return HttpResponse('没有需要上传的文件！')
+        else:
+            cre_mul_upload = client.create_multipart_upload(Bucket=bucket, Key=str(objFile))
+            upload = client.upload_part(Bucket=bucket, Key=str(objFile), UploadId=cre_mul_upload["UploadId"], PartNumber=1, Body=objFile)
+            com_mul_upload = client.complete_multipart_upload(Bucket=bucket, Key=str(objFile), UploadId=cre_mul_upload["UploadId"],
+                                                              MultipartUpload={ 'Parts': [{'ETag': upload['ETag'], 'PartNumber': 1},]})
  #       file_obj = s3.Bucket(bucket).put_object(Key=objFile, Body=data)
-        down_url = client.generate_presigned_url(
-            'get_object', Params={'Bucket': bucket, 'Key': objFile}, ExpiresIn=360000
-        )
+            down_url = client.generate_presigned_url(
+                'get_object', Params={'Bucket': bucket, 'Key': str(objFile)}, ExpiresIn=360000
+            )
     for bucket in s3.buckets.all():
         bucket_list.append(bucket.name)
     return render(request, "index.html", {"name": bucket_list, "url": json.dumps(down_url)})
